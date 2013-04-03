272c936c8925ba57fa3e3363d2dc892e53af467b
Tran Hoang Hiep <hiepgia@hiepgia.com>
2013-04-03 11:14:04 +0700

    Update work

--- src/vn/com/amdvietnam/rma/LoginActivity.java
+++ src/vn/com/amdvietnam/rma/LoginActivity.java
@@ -1,8 +1,10 @@
 package vn.com.amdvietnam.rma;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +17,7 @@ public class LoginActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		
+		//Start Login Processing
 		final Button btnLogin = (Button) findViewById(R.id.btnLogin);
 		btnLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -26,7 +29,10 @@ public class LoginActivity extends Activity {
             	alertDialog.show();
             }
         });
-        
+		//End Login Processing
+		
+		
+		//Start switch to register activity
         final TextView uriRegister = (TextView) findViewById(R.id.uriRegister);
         uriRegister.setOnClickListener(new View.OnClickListener() {
 			
@@ -36,9 +42,19 @@ public class LoginActivity extends Activity {
 				
 			}
 		});
+        //End Switch to register activity
         
-        //Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
-        //startActivity(browserIntent);
+        //Start Open Link AMDVietNam
+        final TextView uriAMDVietNam = (TextView) findViewById(R.id.linkAMDVietNam);
+        uriAMDVietNam.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://amdvietnam.com.vn"));
+		        startActivity(browserIntent);	
+			}
+		});
+        //End Open Link AMDVietNam
 	}
 
 	@Override
